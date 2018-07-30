f03ecbac2fec4bb10a91ce70d017fb2632c2ceae
Harry Cresswell <studio@harrycresswell.com>
2018-07-30 18:38:35 +0100

    feat(wp-sync-db): added sync db plugin to composer.json and fixed logo svg

--- site/web/app/themes/ft/resources/views/partials/header.blade.php
+++ site/web/app/themes/ft/resources/views/partials/header.blade.php
@@ -11,7 +11,7 @@
 
   </div>
 
-  <div class="tc center mt4"><a class="brand" href="{{ home_url('/') }}"><img src="@asset('images/foodtactics.svg')" alt="Foodtactics" style="height: 90px; width: 260px"></a></div>
+  <div class="tc center mt4"><a class="brand" href="{{ home_url('/') }}"><img src="@asset('images/foodtactics.svg')" alt="Foodtactics" style="height: 90px;"></a></div>
   <nav class="nav-primary mt3">
     @if (has_nav_menu('primary_navigation'))
       {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
